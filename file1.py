c64d18df821e3567593dc75715df2ab58fdaf11c
archit1197 <archit1197@gmail.com>
2017-04-01 18:47:42 +0530

    accuracy 90%

--- file1.py
+++ file1.py
@@ -55,3 +55,12 @@ for name, model in models:
 	names.append(name)
 	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
 	print(msg)
+
+
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
